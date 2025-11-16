926199a13ab1f3581ee99ea626a8c8dc49bbc621
정재원 <jaewon-535>
2025-11-16 17:02:41 +0900

    lab11 solution

---
+++ geo/utils.py
@@ -0,0 +1,7 @@
+import math
+
+def hypotenuse(a, b):
+    return math.sqrt(a*a + b*b)
+
+def circle_area(r):
+    return math.pi * r * r
---
+++ tester.py
@@ -0,0 +1,7 @@
+from geo import utils
+
+c = utils.hypotenuse(3, 4)
+print(f"c = {c}")
+
+area = utils.circle_area(10)
+print(f"area = {area}")
